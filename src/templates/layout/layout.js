91fad30503141f1dd9e5d7109876fb862d67516e
Lcfvs <lcf.vs@hotmail.com>
2021-09-04 23:14:57 +0200

    anticore-server@0.16.4

--- src/templates/layout/layout.js
+++ src/templates/layout/layout.js
@@ -1,7 +1,11 @@
+import { readFile } from 'fs/promises'
 import template from '../../lib/template.js'
 import ping from '../sse/ping/ping.js'
 
+const importmap = `${await readFile('./importmap.json')}`
+
 export default await template(import.meta, {
+  importmap,
   ping,
   view: null
 })
